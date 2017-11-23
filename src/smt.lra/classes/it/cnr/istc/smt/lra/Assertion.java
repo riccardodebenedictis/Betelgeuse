c49add82deee0bc845c82fa9018fee00792f5966
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2017-11-23 17:27:45 +0100

    added propagators for linear arithmetic assertions and tableau rows..

--- src/smt.lra/classes/it/cnr/istc/smt/lra/Assertion.java
+++ src/smt.lra/classes/it/cnr/istc/smt/lra/Assertion.java
@@ -16,9 +16,128 @@
  */
 package it.cnr.istc.smt.lra;
 
+import it.cnr.istc.common.InfRational;
+import it.cnr.istc.smt.Lit;
+import java.util.Collection;
+
 /**
+ * This class is used for representing assertions of the linear real arithmetic
+ * (LRA) theory.
  *
  * @author Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
  */
 class Assertion {
+
+    private final LRATheory th;
+    final Op op;
+    final int b; // the controlling variable..
+    final int x; // the constrained (numeric) variable..
+    final InfRational v; // the value..
+
+    Assertion(final LRATheory th, final Op op, final int b, final int x, final InfRational v) {
+        this.th = th;
+        this.op = op;
+        this.b = b;
+        this.x = x;
+        this.v = v;
+    }
+
+    /**
+     * Notifies, for propagation purposes, this assertion that the lower bound
+     * of variable {@code x_i} has changed.
+     *
+     * @param x_i the variable whose lower bound has changed.
+     * @param cnfl the conflict clause in case propagation fails.
+     * @return {@code true} if propagation succeeds.
+     */
+    boolean propagate_lb(final int x_i, final Collection<Lit> cnfl) {
+        assert cnfl.isEmpty();
+        if (th.lb(x_i).gt(v)) {
+            switch (op) {
+                case LEq: // the assertion is unsatisfable: [x_i >= lb(x_i)] -> ![x_i <= v]..
+                    switch (th.sat_core.value(b)) {
+                        case True:                                                          // we have a propositional inconsistency..
+                            cnfl.add(new Lit(b, false));                                    // either the literal 'b' is false ..
+                            cnfl.add(th.assigns.get(LRATheory.lb_index(x_i)).reason.not()); // or what asserted the lower bound is false..
+                            return false;
+                        case Undefined: // we propagate information to the sat core..
+                            th.sat_core.record(new Lit(b, false), th.assigns.get(LRATheory.lb_index(x_i)).reason.not());
+                    }
+                    break;
+                case GEq: // the assertion is satisfied; [x_i >= lb(x_i)] -> [x_i >= v]..
+                    switch (th.sat_core.value(b)) {
+                        case False:                                                         // we have a propositional inconsistency..
+                            cnfl.add(new Lit(b));                                           // either the literal 'b' is true ..
+                            cnfl.add(th.assigns.get(LRATheory.lb_index(x_i)).reason.not()); // or what asserted the lower bound is false..
+                            return false;
+                        case Undefined: // we propagate information to the sat core..
+                            th.sat_core.record(new Lit(b), th.assigns.get(LRATheory.lb_index(x_i)).reason.not());
+                    }
+                    break;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Notifies, for propagation purposes, this assertion that the upper bound
+     * of variable {@code x_i} has changed.
+     *
+     * @param x_i the variable whose upper bound has changed.
+     * @param cnfl the conflict clause in case propagation fails.
+     * @return {@code true} if propagation succeeds.
+     */
+    boolean propagate_ub(final int x_i, final Collection<Lit> cnfl) {
+        assert cnfl.isEmpty();
+        if (th.ub(x_i).lt(v)) {
+            switch (op) {
+                case LEq: // the assertion is satisfied: [x_i <= ub(x_i)] -> [x_i <= v]..
+                    switch (th.sat_core.value(b)) {
+                        case False:                                                         // we have a propositional inconsistency..
+                            cnfl.add(new Lit(b));                                           // either the literal 'b' is true ..
+                            cnfl.add(th.assigns.get(LRATheory.ub_index(x_i)).reason.not()); // or what asserted the upper bound is false..
+                            return false;
+                        case Undefined: // we propagate information to the sat core..
+                            th.sat_core.record(new Lit(b), th.assigns.get(LRATheory.ub_index(x_i)).reason.not());
+                    }
+                    break;
+                case GEq: // the assertion is unsatisfable; [x_i <= ub(x_i)] -> ![x_i >= v]..
+                    switch (th.sat_core.value(b)) {
+                        case True:                                                          // we have a propositional inconsistency..
+                            cnfl.add(new Lit(b, false));                                    // either the literal 'b' is false ..
+                            cnfl.add(th.assigns.get(LRATheory.ub_index(x_i)).reason.not()); // or what asserted the upper bound is false..
+                            return false;
+                        case Undefined: // we propagate information to the sat core..
+                            th.sat_core.record(new Lit(b, false), th.assigns.get(LRATheory.ub_index(x_i)).reason.not());
+                    }
+                    break;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[b").append(b).append("] ");
+        sb.append("x").append(x);
+        switch (op) {
+            case LEq:
+                sb.append(" <= ");
+                break;
+            case GEq:
+                sb.append(" >= ");
+                break;
+            default:
+                throw new AssertionError(op.name());
+        }
+        sb.append(v.toString());
+        return sb.toString();
+    }
+
+    private enum Op {
+        LEq, GEq
+    }
 }
--- src/smt.lra/classes/it/cnr/istc/smt/lra/Row.java
+++ src/smt.lra/classes/it/cnr/istc/smt/lra/Row.java
@@ -16,9 +16,269 @@
  */
 package it.cnr.istc.smt.lra;
 
+import it.cnr.istc.common.InfRational;
+import it.cnr.istc.common.Lin;
+import it.cnr.istc.common.Rational;
+import it.cnr.istc.smt.Lit;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
 /**
+ * This class is used for representing tableau rows.
  *
  * @author Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
  */
 class Row {
+
+    private final LRATheory th;
+    private final int x; // the basic variable..
+    private final Lin l; // the linear expression..
+
+    Row(final LRATheory th, final int x, final Lin l) {
+        this.th = th;
+        this.x = x;
+        this.l = l;
+    }
+
+    /**
+     * Notifies, for propagation purposes, this tableau row that the lower bound
+     * of variable {@code x_i} has changed.
+     *
+     * @param x_i the variable whose lower bound has changed.
+     * @param cnfl the conflict clause in case propagation fails.
+     * @return {@code true} if propagation succeeds.
+     */
+    boolean propagate_lb(final int x_i, final Collection<Lit> cnfl) {
+        assert cnfl.isEmpty();
+        // we make room for the first literal..
+        cnfl.add(new Lit(0));
+        if (l.getVars().get(x_i).isPositive()) {
+            InfRational lb = new InfRational();
+            for (Map.Entry<Integer, Rational> term : l.getVars().entrySet()) {
+                if (term.getValue().isPositive()) {
+                    if (th.lb(term.getKey()).getRational().isNegativeInfinite()) {
+                        // nothing to propagate..
+                        cnfl.clear();
+                        return true;
+                    } else {
+                        lb.add(th.lb(term.getKey()).times(term.getValue()));
+                        cnfl.add(th.assigns.get(LRATheory.lb_index(term.getKey())).reason.not());
+                    }
+                } else if (term.getValue().isNegative()) {
+                    if (th.ub(term.getKey()).getRational().isPositiveInfinite()) {
+                        // nothing to propagate..
+                        cnfl.clear();
+                        return true;
+                    } else {
+                        lb.add(th.ub(term.getKey()).times(term.getValue()));
+                        cnfl.add(th.assigns.get(LRATheory.ub_index(term.getKey())).reason.not());
+                    }
+                }
+            }
+
+            if (lb.gt(th.lb(x))) {
+                for (Assertion c : th.a_watches.get(x)) {
+                    if (lb.gt(c.v)) {
+                        switch (c.op) {
+                            case LEq: // the assertion is unsatisfable..
+                                ((List<Lit>) cnfl).set(0, new Lit(c.b, false));
+                                switch (th.sat_core.value(c.b)) {
+                                    case True: // we have a propositional inconsistency..
+                                        return false;
+                                    case Undefined: // we propagate information to the sat core..
+                                        th.sat_core.record(cnfl.toArray(new Lit[cnfl.size()]));
+                                }
+                                break;
+                            case GEq: // the assertion is satisfied..
+                                ((List<Lit>) cnfl).set(0, new Lit(c.b));
+                                switch (th.sat_core.value(c.b)) {
+                                    case False: // we have a propositional inconsistency..
+                                        return false;
+                                    case Undefined: // we propagate information to the sat core..
+                                        th.sat_core.record(cnfl.toArray(new Lit[cnfl.size()]));
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+        } else {
+            InfRational ub = new InfRational();
+            for (Map.Entry<Integer, Rational> term : l.getVars().entrySet()) {
+                if (term.getValue().isPositive()) {
+                    if (th.ub(term.getKey()).getRational().isPositiveInfinite()) {
+                        // nothing to propagate..
+                        cnfl.clear();
+                        return true;
+                    } else {
+                        ub.add(th.ub(term.getKey()).times(term.getValue()));
+                        cnfl.add(th.assigns.get(LRATheory.ub_index(term.getKey())).reason.not());
+                    }
+                } else if (term.getValue().isNegative()) {
+                    if (th.lb(term.getKey()).getRational().isNegativeInfinite()) {
+                        // nothing to propagate..
+                        cnfl.clear();
+                        return true;
+                    } else {
+                        ub.add(th.lb(term.getKey()).times(term.getValue()));
+                        cnfl.add(th.assigns.get(LRATheory.lb_index(term.getKey())).reason.not());
+                    }
+                }
+            }
+
+            if (ub.lt(th.ub(x))) {
+                for (Assertion c : th.a_watches.get(x)) {
+                    if (ub.lt(c.v)) {
+                        switch (c.op) {
+                            case LEq: // the assertion is satisfied..
+                                ((List<Lit>) cnfl).set(0, new Lit(c.b));
+                                switch (th.sat_core.value(c.b)) {
+                                    case False: // we have a propositional inconsistency..
+                                        return false;
+                                    case Undefined: // we propagate information to the sat core..
+                                        th.sat_core.record(cnfl.toArray(new Lit[cnfl.size()]));
+                                }
+                                break;
+                            case GEq: // the assertion is unsatisfable..
+                                ((List<Lit>) cnfl).set(0, new Lit(c.b, false));
+                                switch (th.sat_core.value(c.b)) {
+                                    case True: // we have a propositional inconsistency..
+                                        return false;
+                                    case Undefined: // we propagate information to the sat core..
+                                        th.sat_core.record(cnfl.toArray(new Lit[cnfl.size()]));
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        cnfl.clear();
+        return true;
+    }
+
+    /**
+     * Notifies, for propagation purposes, this tableau row that the upper bound
+     * of variable {@code x_i} has changed.
+     *
+     * @param x_i the variable whose upper bound has changed.
+     * @param cnfl the conflict clause in case propagation fails.
+     * @return {@code true} if propagation succeeds.
+     */
+    boolean propagate_ub(final int x_i, final Collection<Lit> cnfl) {
+        assert cnfl.isEmpty();
+        // we make room for the first literal..
+        cnfl.add(new Lit(0));
+        if (l.getVars().get(x_i).isPositive()) {
+            InfRational ub = new InfRational();
+            for (Map.Entry<Integer, Rational> term : l.getVars().entrySet()) {
+                if (term.getValue().isPositive()) {
+                    if (th.ub(term.getKey()).getRational().isPositiveInfinite()) {
+                        // nothing to propagate..
+                        cnfl.clear();
+                        return true;
+                    } else {
+                        ub.add(th.ub(term.getKey()).times(term.getValue()));
+                        cnfl.add(th.assigns.get(LRATheory.ub_index(term.getKey())).reason.not());
+                    }
+                } else if (term.getValue().isNegative()) {
+                    if (th.lb(term.getKey()).getRational().isNegativeInfinite()) {
+                        // nothing to propagate..
+                        cnfl.clear();
+                        return true;
+                    } else {
+                        ub.add(th.lb(term.getKey()).times(term.getValue()));
+                        cnfl.add(th.assigns.get(LRATheory.lb_index(term.getKey())).reason.not());
+                    }
+                }
+            }
+
+            if (ub.lt(th.ub(x))) {
+                for (Assertion c : th.a_watches.get(x)) {
+                    if (ub.lt(c.v)) {
+                        switch (c.op) {
+                            case LEq: // the assertion is satisfied..
+                                ((List<Lit>) cnfl).set(0, new Lit(c.b));
+                                switch (th.sat_core.value(c.b)) {
+                                    case False: // we have a propositional inconsistency..
+                                        return false;
+                                    case Undefined: // we propagate information to the sat core..
+                                        th.sat_core.record(cnfl.toArray(new Lit[cnfl.size()]));
+                                }
+                                break;
+                            case GEq: // the assertion is unsatisfable..
+                                ((List<Lit>) cnfl).set(0, new Lit(c.b, false));
+                                switch (th.sat_core.value(c.b)) {
+                                    case True: // we have a propositional inconsistency..
+                                        return false;
+                                    case Undefined: // we propagate information to the sat core..
+                                        th.sat_core.record(cnfl.toArray(new Lit[cnfl.size()]));
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+        } else {
+            InfRational lb = new InfRational();
+            for (Map.Entry<Integer, Rational> term : l.getVars().entrySet()) {
+                if (term.getValue().isPositive()) {
+                    if (th.lb(term.getKey()).getRational().isNegativeInfinite()) {
+                        // nothing to propagate..
+                        cnfl.clear();
+                        return true;
+                    } else {
+                        lb.add(th.lb(term.getKey()).times(term.getValue()));
+                        cnfl.add(th.assigns.get(LRATheory.lb_index(term.getKey())).reason.not());
+                    }
+                } else if (term.getValue().isNegative()) {
+                    if (th.ub(term.getKey()).getRational().isPositiveInfinite()) {
+                        // nothing to propagate..
+                        cnfl.clear();
+                        return true;
+                    } else {
+                        lb.add(th.ub(term.getKey()).times(term.getValue()));
+                        cnfl.add(th.assigns.get(LRATheory.ub_index(term.getKey())).reason.not());
+                    }
+                }
+            }
+
+            if (lb.gt(th.lb(x))) {
+                for (Assertion c : th.a_watches.get(x)) {
+                    if (lb.gt(c.v)) {
+                        switch (c.op) {
+                            case LEq: // the assertion is unsatisfable..
+                                ((List<Lit>) cnfl).set(0, new Lit(c.b, false));
+                                switch (th.sat_core.value(c.b)) {
+                                    case True: // we have a propositional inconsistency..
+                                        return false;
+                                    case Undefined: // we propagate information to the sat core..
+                                        th.sat_core.record(cnfl.toArray(new Lit[cnfl.size()]));
+                                }
+                                break;
+                            case GEq: // the assertion is satisfied..
+                                ((List<Lit>) cnfl).set(0, new Lit(c.b));
+                                switch (th.sat_core.value(c.b)) {
+                                    case False: // we have a propositional inconsistency..
+                                        return false;
+                                    case Undefined: // we propagate information to the sat core..
+                                        th.sat_core.record(cnfl.toArray(new Lit[cnfl.size()]));
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        cnfl.clear();
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "x" + x + " == " + l.toString();
+    }
 }
